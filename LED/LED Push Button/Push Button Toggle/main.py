f1e529256a3024ea9e52de3558740da316a4baaf
Jacob Waters <148648806+Jpwaters09@users.noreply.github.com>
2023-10-26 22:30:23 +0100

    Create main.py

---
+++ LED/LED Push Button/Push Button Toggle/main.py
@@ -0,0 +1,17 @@
+###########################################
+# LED Push Button Toggle                  #
+# Author: Jacob Waters                    #
+# Github: github.com/jpwaters09           #
+# Contact me: jpwaters.github@gmail.com   #
+###########################################
+
+from gpiozero import LED, Button
+from time import sleep
+
+button = Button(20)
+led = LED(21)
+
+while True:
+  if button.value == 1:
+    led.toggle()
+    sleep(0.3)
