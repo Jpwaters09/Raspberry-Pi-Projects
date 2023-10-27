fc59f8768e9b2431875c2776ccd4b2fd00d874de
Jacob Waters <148648806+Jpwaters09@users.noreply.github.com>
2023-10-27 12:46:11 +0100

    Create main.py

---
+++ LED/RGB LED/main.py
@@ -0,0 +1,63 @@
+###########################################
+# RGB LED                                 #
+# Author: Jacob Waters                    #
+# Github: github.com/jpwaters09           #
+# Contact me: jpwaters.github@gmail.com   #
+###########################################
+
+from gpiozero import LED
+from time import sleep
+
+##########Change the number below to change the delay in seconds##########
+delay = 0.5                                                              #
+##########################################################################
+
+r = LED(21)
+g = LED(20)
+b = LED(16)
+
+def rg():
+  r.on()
+  g.on()
+
+def rb():
+  r.on()
+  b.on()
+
+def gb():
+  g.on()
+  b.on()
+
+def r():
+  r.on()
+
+def g():
+  g.on()
+
+def b():
+  b.on()
+
+def off():
+  r.off()
+  g.off()
+  b.off()
+
+while True:
+  r()
+  sleep(delay)
+  off()
+  g()
+  sleep(delay)
+  off()
+  b()
+  sleep(delay)
+  off()
+  rg()
+  sleep(delay)
+  off()
+  rb()
+  sleep(delay)
+  off()
+  gb()
+  sleep(delay)
+  off()
