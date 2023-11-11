330b64f1d296e642ab3ee1a036e07e53375cf84c
Jacob Waters <148648806+Jpwaters09@users.noreply.github.com>
2023-11-11 15:29:57 +0000

    Update main.py

--- LED/LED Blink/main.py
+++ LED/LED Blink/main.py
@@ -9,7 +9,7 @@
 from gpiozero import LED
 from time import sleep
 
-##########Change the number below to change the delay in seconds##########
+########## Change the number below to change the delay in seconds ##########
 delay = 0.5                                                              #
 ##########################################################################
 
