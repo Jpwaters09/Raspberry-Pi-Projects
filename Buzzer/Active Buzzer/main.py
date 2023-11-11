3325d8e6626f9bc3a13c9587fdaebbd930139689
Jacob Waters <148648806+Jpwaters09@users.noreply.github.com>
2023-11-11 15:27:35 +0000

    Update main.py

--- Buzzer/Active Buzzer/main.py
+++ Buzzer/Active Buzzer/main.py
@@ -1,9 +1,10 @@
-###########################################
-# Active Buzzer                           #
-# Author: Jacob Waters                    #
-# Github: github.com/jpwaters09           #
-# Contact me: jpwaters.github@gmail.com   #
-###########################################
+##############################################
+# Active Buzzer                              #
+# Author: Jacob Waters                       #
+# Github: github.com/jpwaters09              #
+# Contact me: jpwaters.github@gmail.com      #
+# Follow my WhatsApp Channel: bit.ly/3sr99ZO #
+##############################################
 
 from gpiozero import Buzzer, Button
 
