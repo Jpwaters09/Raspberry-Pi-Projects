9e6f369c958f264ec558047465f732cfd908e461
Jacob Waters <148648806+Jpwaters09@users.noreply.github.com>
2023-11-11 15:23:54 +0000

    Update main.py

--- LED/LED Push Button/Push Button Toggle/main.py
+++ LED/LED Push Button/Push Button Toggle/main.py
@@ -1,9 +1,10 @@
-###########################################
-# LED Push Button Toggle                  #
-# Author: Jacob Waters                    #
-# Github: github.com/jpwaters09           #
-# Contact me: jpwaters.github@gmail.com   #
-###########################################
+##############################################
+# LED Push Button Toggle                     #
+# Author: Jacob Waters                       #
+# Github: github.com/jpwaters09              #
+# Contact me: jpwaters.github@gmail.com      #
+# Follow my WhatsApp Channel: bit.ly/3sr99ZO #
+##############################################
 
 from gpiozero import LED, Button
 from time import sleep
