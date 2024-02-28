e57b2b154cd5e78d681f07cbd3bc494100af7ae3
Jacob Waters <jpwaters.github@gmail.com>
2024-02-28 19:46:24 +0000

    Update main.py

--- LED/LED Blink/main.py
+++ LED/LED Blink/main.py
@@ -1,10 +1,11 @@
-##############################################
-# LED Blink                                  #
-# Author: Jacob Waters                       #
-# Github: github.com/jpwaters09              #
-# Contact me: jpwaters.github@gmail.com      #
-# Follow my WhatsApp Channel: bit.ly/3sr99ZO #
-##############################################
+################################################
+## LED Blink                                  ##
+## Language: Python                           ##
+## Author: Jacob Waters                       ##
+## Github: github.com/jpwaters09              ##
+## Contact me: jpwaters.github@gmail.com      ##
+## Follow my WhatsApp Channel: bit.ly/3sr99ZO ##
+################################################
 
 from gpiozero import LED
 from time import sleep
