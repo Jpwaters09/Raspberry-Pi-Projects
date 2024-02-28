6a72fae4a086dcb2124087c36d463a8f4b76530c
Jacob Waters <jpwaters.github@gmail.com>
2024-02-28 19:47:48 +0000

    Update main.py

--- LED/RGB LED/main.py
+++ LED/RGB LED/main.py
@@ -1,10 +1,11 @@
-##############################################
-# RGB LED                                    #
-# Author: Jacob Waters                       #
-# Github: github.com/jpwaters09              #
-# Contact me: jpwaters.github@gmail.com      #
-# Follow my WhatsApp Channel: bit.ly/3sr99ZO #
-##############################################
+################################################
+## RGB LED                                    ##
+## Language: Python                           ##
+## Author: Jacob Waters                       ##
+## Github: github.com/jpwaters09              ##
+## Contact me: jpwaters.github@gmail.com      ##
+## Follow my WhatsApp Channel: bit.ly/3sr99ZO ##
+################################################
 
 from gpiozero import LED
 from time import sleep
