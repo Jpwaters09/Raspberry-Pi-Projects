da4786cea9c56b1bad524333fd979a0057f1183b
Jacob Waters <jpwaters.github@gmail.com>
2024-05-05 17:03:39 +0100

    Update main.py

--- Buzzer/Passive Buzzer/main.py
+++ Buzzer/Passive Buzzer/main.py
@@ -10,36 +10,40 @@ from gpiozero import TonalBuzzer, Button
 from gpiozero.tones import Tone
 from time import sleep
 
+########## Change the number below to change the delay in seconds ##########
+delay = 0.05                                                               #
+############################################################################
+
 buzzer = TonalBuzzer(21)
 button = Button(20)
 
 def alertor():
   buzzer.play(Tone(midi=57))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=59))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=61))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=63))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=65))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=67))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=69))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=71))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=73))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=75))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=77))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=79))
-  sleep(0.1)
+  sleep(delay)
   buzzer.play(Tone(midi=81))
-  sleep(0.1)
+  sleep(delay)
 
 while True:
   if button.value == 1:
