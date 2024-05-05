0a19c025c463cb9e619b41e00417d5a5e378eab0
Jacob Waters <jpwaters.github@gmail.com>
2024-05-05 17:02:02 +0100

    Update main.py

--- Buzzer/Passive Buzzer/main.py
+++ Buzzer/Passive Buzzer/main.py
@@ -8,21 +8,41 @@
 
 from gpiozero import TonalBuzzer, Button
 from gpiozero.tones import Tone
-import math
 from time import sleep
 
 buzzer = TonalBuzzer(21)
 button = Button(20)
 
 def alertor():
-  for x in range(0, 361):
-    sinVal = Math.sin(x * (Math.pi / 18.0))
-    toneVal = 2000 + sinVal * 500
-    b.play(Tone(toneVal))
-    sleep(0.001)
+  buzzer.play(Tone(midi=57))
+  sleep(0.1)
+  buzzer.play(Tone(midi=59))
+  sleep(0.1)
+  buzzer.play(Tone(midi=61))
+  sleep(0.1)
+  buzzer.play(Tone(midi=63))
+  sleep(0.1)
+  buzzer.play(Tone(midi=65))
+  sleep(0.1)
+  buzzer.play(Tone(midi=67))
+  sleep(0.1)
+  buzzer.play(Tone(midi=69))
+  sleep(0.1)
+  buzzer.play(Tone(midi=71))
+  sleep(0.1)
+  buzzer.play(Tone(midi=73))
+  sleep(0.1)
+  buzzer.play(Tone(midi=75))
+  sleep(0.1)
+  buzzer.play(Tone(midi=77))
+  sleep(0.1)
+  buzzer.play(Tone(midi=79))
+  sleep(0.1)
+  buzzer.play(Tone(midi=81))
+  sleep(0.1)
 
 while True:
   if button.value == 1:
     alertor()
-  else:
+  if button.value == 0:
     buzzer.stop()
