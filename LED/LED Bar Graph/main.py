9a276f920aac58edffba173821ffd19b2136eb43
Jacob Waters <jpwaters.github@gmail.com>
2024-07-29 22:16:48 +0100

    Update main.py

--- LED/LED Bar Graph/main.py
+++ LED/LED Bar Graph/main.py
@@ -3,6 +3,7 @@
 ## Language: Python                           ##
 ## Author: Jacob Waters                       ##
 ## Github: github.com/jpwaters09              ##
+## Copyright (c) 2024 Jacob Waters            ##
 ## Contact me: jpwaters.github@gmail.com      ##
 ################################################
 
